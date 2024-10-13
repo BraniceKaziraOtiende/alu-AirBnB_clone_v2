3321ae660498013aaf056aebe446a4c77d1bdefc
BraniceKaziraOtiende <branicekazira@gmail.com>
2024-10-13 20:46:39 +0000

    Files execution

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,14 +2,14 @@
 """
 This is a simple Flask web application.
 
-It defines two routes:
+It defines three routes:
 - Route for the root URL ("/") that displays "Hello HBNB!"
 - Route for "/hbnb" that displays "HBNB"
+- Route for "/c/<text>" that displays "C <text>" where <text> can be any string
 
 Requirements:
 - Your web application must be listening on 0.0.0.0, port 5000
 """
-
 
 from flask import Flask
 
@@ -26,6 +26,12 @@ def index():
 def about():
     """Route for "/hbnb"."""
     return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C(text):
+    """Route for "/c/<text>"."""
+    return "C %s" % text.replace('_', ' ')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -7,8 +7,9 @@ It defines four routes:
 - Route for "/hbnb" that displays "HBNB"
 - Route for "/c/<text>" that displays "C <text>" where <text> can be any string
 - Route for "/python/(<text>)" that displays "Python <text>"
-- Route for the "/number/<n>" URL.
-- Route for the "/number_template/<n>" URL.
+- Route for /number/<n> URL.
+- Route for /number_template/<n> URL.
+- Route for /number_odd_or_even/<n> URL.
 
 Requirements:
 - Your web application must be listening on 0.0.0.0, port 5000
@@ -27,27 +28,27 @@ def index():
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Route for the /hbnb URL."""
+def about():
+    """Route for /hbnb URL."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Route for the /c/<text> URL."""
+def C(text):
+    """Route for /c/<text> URL."""
     return "C %s" % text.replace('_', ' ')
 
 
-@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python(text):
-    """Route for the /python and /python/<text> URLs."""
+def python(text="is cool"):
+    """Route for /python URL."""
     return "Python %s" % text.replace('_', ' ')
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Route for the /number/<n> URL. """
+    """Route for the /number/<n> URL."""
     return "%d is a number" % n
 
 
@@ -55,6 +56,12 @@ def number(n):
 def number_template(n):
     """Route for the /number_template/<n> URL."""
     return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """Route for the /number_odd_or_even/<n> URL."""
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
